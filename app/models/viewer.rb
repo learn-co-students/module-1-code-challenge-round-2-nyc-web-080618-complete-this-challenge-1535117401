1d4fc1d4f9960bb0f919bf09cf8b191917b92022
mguevara717 <marissa.guevara717@gmail.com>
2018-08-24 10:33:31 -0400

    Flamingo Friyays

--- app/models/viewer.rb
+++ app/models/viewer.rb
@@ -1,15 +1,40 @@
 class Viewer
-  attr_accessor :username
+  attr_reader :username
+  attr_accessor :rating
+
 
   @@all = []
 
-  def initialize(username)
+  def initialize(username, rating)
     @username = username
-    self.class.all << self
+    @rating = rating
+    @@all << self
   end
 
   def self.all
     @@all
   end
-  
-end
+
+  def queue_items
+    arr = QueueItem.all.select do |queue_item|
+      queue_item.viewer == self
+    end
+    arr
+  end
+
+  def queue_movies
+    arr = queue_items.map do |queue_item|
+      queue_item.movie
+    end
+    arr
+  end
+
+  def add_movie_to_queue(movie)
+    QueueItem.new(self, movie)
+  end
+
+
+
+
+
+end#END OF CLASS
